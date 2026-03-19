451a888f118fdca377fdd3895fe3bc90f42a8ddd
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-03-18 22:16:47 -0300

    feat: apply PHP language adjustments and add Mailtrap configuration

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'Celke') }}</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico.png" sizes="any" />
+
+        @routes
+        @vite(['resources/js/app.ts'])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
+</html>
